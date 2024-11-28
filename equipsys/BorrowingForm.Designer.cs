081ae212f291048f0252bc7c366d7248a098dae8
Grace <gracelozanolee@gmail.com>
2024-11-28 20:58:37 +0800

    Added first name, last name, student id etc.

--- equipsys/BorrowingForm.Designer.cs
+++ equipsys/BorrowingForm.Designer.cs
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            button7 = new Button();
+            label10 = new Label();
+            button6 = new Button();
+            label9 = new Label();
+            button5 = new Button();
+            label8 = new Label();
+            button4 = new Button();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
+            label6 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            label6 = new Label();
-            comboBox1 = new ComboBox();
-            label7 = new Label();
-            button4 = new Button();
-            label8 = new Label();
-            button5 = new Button();
-            label9 = new Label();
-            button6 = new Button();
-            label10 = new Label();
-            button7 = new Button();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
@@ -72,9 +72,147 @@
             groupBox1.ForeColor = Color.ForestGreen;
             groupBox1.Location = new Point(135, 57);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(653, 204);
+            groupBox1.Size = new Size(663, 204);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(486, 38);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 16;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(486, 16);
+            label10.Name = "label10";
+            label10.Size = new Size(118, 20);
+            label10.TabIndex = 15;
+            label10.Text = "Contact Number";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(314, 129);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 14;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(314, 98);
+            label9.Name = "label9";
+            label9.Size = new Size(46, 20);
+            label9.TabIndex = 13;
+            label9.Text = "Email";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(372, 38);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 12;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(372, 15);
+            label8.Name = "label8";
+            label8.Size = new Size(75, 20);
+            label8.TabIndex = 11;
+            label8.Text = "Year Level";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(191, 129);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 10;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(191, 98);
+            label7.Name = "label7";
+            label7.Size = new Size(54, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Course";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "College of Education", "College of Liberal Arts and Sciences", "College of Business and Accountancy", "College of Criminal Justice Education" });
+            comboBox1.Location = new Point(11, 130);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(11, 98);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 20);
+            label6.TabIndex = 7;
+            label6.Text = "College";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(252, 38);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 5;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(131, 38);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(11, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(252, 15);
+            label5.Name = "label5";
+            label5.Size = new Size(79, 20);
+            label5.TabIndex = 2;
+            label5.Text = "Student ID";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(131, 15);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Last Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 20);
+            label3.TabIndex = 0;
+            label3.Text = "First Name";
             // 
             // label1
             // 
@@ -94,144 +232,6 @@
             label2.Size = new Size(144, 32);
             label2.TabIndex = 2;
             label2.Text = "Borrower";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(11, 15);
-            label3.Name = "label3";
-            label3.Size = new Size(80, 20);
-            label3.TabIndex = 0;
-            label3.Text = "First Name";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(131, 15);
-            label4.Name = "label4";
-            label4.Size = new Size(79, 20);
-            label4.TabIndex = 1;
-            label4.Text = "Last Name";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(252, 15);
-            label5.Name = "label5";
-            label5.Size = new Size(79, 20);
-            label5.TabIndex = 2;
-            label5.Text = "Student ID";
-            // 
-            // button1
-            // 
-            button1.Location = new Point(11, 38);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 3;
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(131, 38);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 4;
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(252, 38);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 29);
-            button3.TabIndex = 5;
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(11, 98);
-            label6.Name = "label6";
-            label6.Size = new Size(60, 20);
-            label6.TabIndex = 7;
-            label6.Text = "College";
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "College of Education", "College of Liberal Arts and Sciences", "College of Business and Accountancy", "College of Criminal Justice Education" });
-            comboBox1.Location = new Point(11, 130);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 8;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(191, 98);
-            label7.Name = "label7";
-            label7.Size = new Size(54, 20);
-            label7.TabIndex = 9;
-            label7.Text = "Course";
-            // 
-            // button4
-            // 
-            button4.Location = new Point(191, 129);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 29);
-            button4.TabIndex = 10;
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(372, 15);
-            label8.Name = "label8";
-            label8.Size = new Size(75, 20);
-            label8.TabIndex = 11;
-            label8.Text = "Year Level";
-            // 
-            // button5
-            // 
-            button5.Location = new Point(372, 38);
-            button5.Name = "button5";
-            button5.Size = new Size(94, 29);
-            button5.TabIndex = 12;
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += button5_Click;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(314, 98);
-            label9.Name = "label9";
-            label9.Size = new Size(46, 20);
-            label9.TabIndex = 13;
-            label9.Text = "Email";
-            // 
-            // button6
-            // 
-            button6.Location = new Point(314, 129);
-            button6.Name = "button6";
-            button6.Size = new Size(94, 29);
-            button6.TabIndex = 14;
-            button6.UseVisualStyleBackColor = true;
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Location = new Point(486, 16);
-            label10.Name = "label10";
-            label10.Size = new Size(118, 20);
-            label10.TabIndex = 15;
-            label10.Text = "Contact Number";
-            // 
-            // button7
-            // 
-            button7.Location = new Point(486, 38);
-            button7.Name = "button7";
-            button7.Size = new Size(94, 29);
-            button7.TabIndex = 16;
-            button7.UseVisualStyleBackColor = true;
             // 
             // Borrowing
             // 
