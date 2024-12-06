8ea0d389764e843952c2ff619da5ad2796d27e4c
Arkmers <141404930+Arkmers@users.noreply.github.com>
2024-12-06 19:00:14 +0800

    GlobalConfig.cs
    globally accessible connection string

---
+++ GlobalConfig.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equipsys
+{
+    public static class GlobalConfig
+    {
+        // replace all connection string for SqlConnection
+        // change this string for your local device's connection string
+        public static string ConnectionString = "Data Source=ARKDRAEDON;Initial Catalog=EquipmentBorrowing;Integrated Security=True;Trust Server Certificate=True";
+    }
