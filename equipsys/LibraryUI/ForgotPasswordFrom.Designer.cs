d1a6ff292f60cac55d55b29578db250e046d01b2
Azelf3600 <151857500+Azelf3600@users.noreply.github.com>
2024-12-20 12:34:56 +0800

    Done IM CHECKING FIXING BUGS

---
+++ equipsys/LibraryUI/ForgotPasswordFrom.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace equipsys.LibraryUI
+{
+    partial class ForgotPasswordFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForgotPasswordFrom));
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            label2 = new Label();
+            StudentIDValue = new TextBox();
+            label11 = new Label();
+            button1 = new Button();
+            ExitBTN = new PictureBox();
+            label7 = new Label();
+            panel3 = new Panel();
+            LastNameValue = new TextBox();
+            label10 = new Label();
+            groupBox1 = new GroupBox();
+            label3 = new Label();
+            RegisterButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ExitBTN).BeginInit();
+            panel3.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(81, 83);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(166, 163);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 50;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Arial Rounded MT Bold", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(75, 477);
+            label4.Name = "label4";
+            label4.Size = new Size(182, 60);
+            label4.TabIndex = 49;
+            label4.Text = "University of Caloocan City\r\n-Congressional Campus\r\n\r\n@mis.ucc.com";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(32, 33, 35);
+            label2.Location = new Point(350, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 20);
+            label2.TabIndex = 23;
+            label2.Text = "Student ID";
+            // 
+            // StudentIDValue
+            // 
+            StudentIDValue.ForeColor = Color.DimGray;
+            StudentIDValue.Location = new Point(350, 106);
+            StudentIDValue.Name = "StudentIDValue";
+            StudentIDValue.Size = new Size(203, 27);
+            StudentIDValue.TabIndex = 6;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Arial Rounded MT Bold", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(78, 247);
+            label11.Name = "label11";
+            label11.Size = new Size(174, 30);
+            label11.TabIndex = 51;
+            label11.Text = "Create an account for you\r\nto explore our system!\r\n";
+            label11.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.DimGray;
+            button1.Location = new Point(174, 482);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 37);
+            button1.TabIndex = 66;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // ExitBTN
+            // 
+            ExitBTN.BackgroundImage = (Image)resources.GetObject("ExitBTN.BackgroundImage");
+            ExitBTN.Cursor = Cursors.Hand;
+            ExitBTN.Location = new Point(629, -18);
+            ExitBTN.Name = "ExitBTN";
+            ExitBTN.Size = new Size(22, 23);
+            ExitBTN.SizeMode = PictureBoxSizeMode.StretchImage;
+            ExitBTN.TabIndex = 72;
+            ExitBTN.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.FromArgb(32, 33, 35);
+            label7.Location = new Point(22, 23);
+            label7.Name = "label7";
+            label7.Size = new Size(120, 20);
+            label7.TabIndex = 44;
+            label7.Text = "Fill up this form";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(32, 33, 35);
+            panel3.Controls.Add(pictureBox1);
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(label11);
+            panel3.Location = new Point(660, -28);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(331, 597);
+            panel3.TabIndex = 71;
+            // 
+            // LastNameValue
+            // 
+            LastNameValue.ForeColor = Color.DimGray;
+            LastNameValue.Location = new Point(58, 108);
+            LastNameValue.Name = "LastNameValue";
+            LastNameValue.Size = new Size(203, 27);
+            LastNameValue.TabIndex = 1;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.FlatStyle = FlatStyle.Flat;
+            label10.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.ForeColor = Color.FromArgb(32, 33, 35);
+            label10.Location = new Point(4, 12);
+            label10.Name = "label10";
+            label10.Size = new Size(157, 35);
+            label10.TabIndex = 67;
+            label10.Text = "Registration";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.WhiteSmoke;
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(StudentIDValue);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(LastNameValue);
+            groupBox1.Controls.Add(label3);
+            groupBox1.ForeColor = SystemColors.ControlText;
+            groupBox1.Location = new Point(34, 93);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(598, 219);
+            groupBox1.TabIndex = 69;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(32, 33, 35);
+            label3.Location = new Point(58, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 20;
+            label3.Text = "Email";
+            // 
+            // RegisterButton
+            // 
+            RegisterButton.BackColor = Color.FromArgb(32, 33, 35);
+            RegisterButton.Cursor = Cursors.Hand;
+            RegisterButton.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            RegisterButton.ForeColor = Color.White;
+            RegisterButton.Location = new Point(310, 482);
+            RegisterButton.Name = "RegisterButton";
+            RegisterButton.Size = new Size(123, 37);
+            RegisterButton.TabIndex = 65;
+            RegisterButton.Text = "Register";
+            RegisterButton.UseVisualStyleBackColor = false;
+            // 
+            // ForgotPasswordFrom
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(995, 541);
+            Controls.Add(button1);
+            Controls.Add(ExitBTN);
+            Controls.Add(panel3);
+            Controls.Add(label10);
+            Controls.Add(groupBox1);
+            Controls.Add(RegisterButton);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ForgotPasswordFrom";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ForgotPasswordFrom";
+            Load += ForgotPasswordFrom_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ExitBTN).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Label label2;
+        private TextBox StudentIDValue;
+        private Label label11;
+        private Button button1;
+        private PictureBox ExitBTN;
+        private Label label7;
+        private Panel panel3;
+        private TextBox LastNameValue;
+        private Label label10;
+        private GroupBox groupBox1;
+        private Label label3;
+        private Button RegisterButton;
+    }
+}
